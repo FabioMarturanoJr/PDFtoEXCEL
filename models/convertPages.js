feefd12bc6eb0299512414ac37d7a393997e7e7f
Fabio Marturano <fabiocmjor@gmail.com>
2021-10-13 16:00:10 -0300

    Conversor 1.0 concluido

--- models/convertPages.js
+++ models/convertPages.js
@@ -1,6 +1,7 @@
 const xl = require('excel4node');
+const { analyzer } = require('./helpers/analyzerEmployee');
 
-const saveToExcel = (employee) => {
+const saveToExcel = (employees) => {
   const headerFile = ['Nome do Trabalhador', 'Rem SEM 13º Sal', 'Rem 13º Sal', 'PIS', 'Base Cal 13º SAL prev', 'Admissao', 'Cont Seg Devida', 'cat', 'ocor Deposito', 'Data/Cod Movimentação', 'cbo', 'jam'];
   const wb = new xl.Workbook();
   const ws = wb.addWorksheet('Sheet 1');
@@ -17,24 +18,80 @@ const saveToExcel = (employee) => {
     ws.cell(1, 1 + index).string(collumnName).style(style);
   });
 
+  // fill employees
+  employees.forEach((employee, index) => {
+    ws.cell(2 + index, 1).string(employee.name).style(style);
+    ws.cell(2 + index, 2).string(employee.remSem).style(style);
+    ws.cell(2 + index, 3).string(employee.rem).style(style);
+    ws.cell(2 + index, 4).string(employee.pis).style(style);
+    ws.cell(2 + index, 5).string(employee.baseSalPrev).style(style);
+    ws.cell(2 + index, 6).string(employee.admissao).style(style);
+    ws.cell(2 + index, 7).string(employee.conSeg).style(style);
+    ws.cell(2 + index, 8).string(employee.cat).style(style);
+    ws.cell(2 + index, 9).string(employee.ocor).style(style);
+    ws.cell(2 + index, 10).string(employee.dataCodMov).style(style);
+    ws.cell(2 + index, 11).string(employee.cbo).style(style);
+    ws.cell(2 + index, 12).string(employee.jam).style(style);
+  });
+
   // create excel file
   wb.write('./EXCEL/Excel.xlsx');
 };
 
 const match =  /NOME TRABALHADOR/i;
 const notMatch =  /LOGRADOURO/i;
+const PIS = /\d{2}.\d{5}.\d{2}-\d/;
+const DATE = "(\\d{2}/\\d{2}/\\d{4})";
+// const NAME = /^[a-zA-Z ]+$/;
 
 const isCorrectPage = (page) => page.some((str) => str.match(match)) 
   && !page.some((str) => str.match(notMatch));
 
+const isPIS = (str) => str.match(PIS);
+const haveDeposito = (str) => str.match(DATE);
+
+const findSeparators = (page) => { 
+  const separetors = [];
+  page.map((str, index) => {
+    if (isPIS(str)) {
+      separetors.push(index);
+    }
+  });
+  return separetors;
+};
+
+const separateEmployees = (page, separetors) => {
+  const employees = [];
+  separetors.forEach((separator, index) => {
+    const startData = haveDeposito(page[separator - 1]) ? separator - 1 : separator;
+    const endSeparator  = haveDeposito(page[separator - 1]) ? separetors[index + 1] - 1 : separetors[index + 1];
+    const endData = separetors[index + 1] - 1 ? endSeparator : page.length;
+
+    const employeeData = page.slice(startData, endData);
+    const employee = { ...analyzer(employeeData.length, employeeData, haveDeposito(page[separator - 1])) };    
+    
+    // console.log(employeeData.length, employeeData);
+    employees.push(employee);
+  });
+  return employees
+};
+
 const convertPages = (pages) => {
+  let employeesData = [];
   for (let i = 0; i < pages.length; i++) {
-    // fintro de paginas ok 
-    console.log('Page', i + 1, isCorrectPage(pages[i]));
-    // criar a remoção das informaçoes nescessárias verificar os campos vazios
+  // for (let i = 64; i < 65; i++) {
+    // fintro de paginas ok
+    if (isCorrectPage(pages[i])) {
+      // criar a remoção das informaçoes nescessárias verificar os campos vazios
+      const separetors = findSeparators(pages[i]);
+      employeesData = [...employeesData, ...separateEmployees(pages[i], separetors)];
+      console.log('pagina', i + 1, isCorrectPage(pages[i]) && 'carregada');
+    }
   }
-  // saveToExcel('Fabio');
-  console.log('total pages', pages.length);
+  // console.log('total funcionarios',  employeesData);
+  // console.log('total funcionarios',  employeesData.length);
+  saveToExcel(employeesData);
+  console.log('arquivo convertido');
 };
 
 module.exports = {
---
+++ models/helpers/analyzerEmployee.js
@@ -0,0 +1,151 @@
+const analyzer = (employeeLength, employeeData, haveDeposito) => {
+  employee = {};
+  if (employeeLength == 12) {
+    employee.name = employeeData[5];
+    employee.remSem = employeeData[6]; // number
+    employee.rem = employeeData[7]; // number
+    employee.pis = employeeData[1];
+    employee.baseSalPrev = employeeData[10]; // number
+    employee.admissao = employeeData[2];
+    employee.conSeg = employeeData[9]; // number
+    employee.cat = employeeData[3];
+    employee.ocor = employeeData[11]; // number
+    employee.dataCodMov = employeeData[0];
+    employee.cbo = employeeData[4];
+    employee.jam = employeeData[8]; // number
+    return employee;
+  }
+
+  if (employeeLength == 13) {
+    employee.name = employeeData[6];
+    employee.remSem = employeeData[7]; // number
+    employee.rem = employeeData[8]; // number
+    employee.pis = employeeData[1];
+    employee.baseSalPrev = employeeData[11]; // number
+    employee.admissao = employeeData[2];
+    employee.conSeg = employeeData[10]; // number
+    employee.cat = employeeData[3];
+    employee.ocor = employeeData[12]; // number
+    employee.dataCodMov = employeeData[0];
+    employee.cbo = employeeData[4];
+    employee.jam = employeeData[9]; // number
+    return employee;
+  }
+
+  if (employeeLength == 7) {
+    employee.name = employeeData[5] + employeeData[6];
+    employee.remSem = '0'; // number
+    employee.rem = '0'; // number
+    employee.pis = employeeData[1];
+    employee.baseSalPrev = '0'; // number
+    employee.admissao = employeeData[2];
+    employee.conSeg = '0'; // number
+    employee.cat = employeeData[3];
+    employee.ocor = '0'; // number
+    employee.dataCodMov = employeeData[0];
+    employee.cbo = employeeData[4];
+    employee.jam = '0'; // number
+    return employee;
+  }
+
+  if (employeeLength == 6) {
+    employee.name = employeeData[5];
+    employee.remSem = '0'; // number
+    employee.rem = '0'; // number
+    employee.pis = employeeData[1];
+    employee.baseSalPrev = '0'; // number
+    employee.admissao = employeeData[2];
+    employee.conSeg = '0'; // number
+    employee.cat = employeeData[3];
+    employee.ocor = '0'; // number
+    employee.dataCodMov = employeeData[0];
+    employee.cbo = employeeData[4];
+    employee.jam = '0'; // number
+    return employee;
+  }
+
+  if (employeeLength == 5) {
+    employee.name = employeeData[4];
+    employee.remSem = '0'; // number
+    employee.rem = '0'; // number
+    employee.pis = employeeData[1];
+    employee.baseSalPrev = '0'; // number
+    employee.admissao = employeeData[2];
+    employee.conSeg = '0'; // number
+    employee.cat = employeeData[3];
+    employee.ocor = '0'; // number
+    employee.dataCodMov = employeeData[0];
+    employee.cbo = '0';
+    employee.jam = '0'; // number
+    return employee;
+  }
+
+  if (employeeLength == 19 && !haveDeposito) {
+    employee.name = employeeData[4];
+    employee.remSem = employeeData[5]; // number
+    employee.rem = employeeData[6]; // number
+    employee.pis = employeeData[0];
+    employee.baseSalPrev = employeeData[9]; // number
+    employee.admissao = employeeData[1];
+    employee.conSeg = employeeData[8]; // number
+    employee.cat = employeeData[2];
+    employee.ocor = employeeData[10]; // number
+    employee.dataCodMov = '0';
+    employee.cbo = employeeData[3];
+    employee.jam = employeeData[7]; // number
+    return employee;
+  }
+
+  if (employeeLength == 18 && !haveDeposito) {
+    employee.name = employeeData[4];
+    employee.remSem = employeeData[5]; // number
+    employee.rem = employeeData[6]; // number
+    employee.pis = employeeData[0];
+    employee.baseSalPrev = employeeData[9]; // number
+    employee.admissao = employeeData[1];
+    employee.conSeg = employeeData[8]; // number
+    employee.cat = employeeData[2];
+    employee.ocor = employeeData[10]; // number
+    employee.dataCodMov = '0';
+    employee.cbo = employeeData[3];
+    employee.jam = employeeData[7]; // number
+    return employee;
+  }
+
+  if (employeeLength == 11 && !haveDeposito) {
+    employee.name = employeeData[4];
+    employee.remSem = employeeData[5]; // number
+    employee.rem = employeeData[6]; // number
+    employee.pis = employeeData[0];
+    employee.baseSalPrev = employeeData[9]; // number
+    employee.admissao = employeeData[1];
+    employee.conSeg = employeeData[8]; // number
+    employee.cat = employeeData[2];
+    employee.ocor = employeeData[10]; // number
+    employee.dataCodMov = '0';
+    employee.cbo = employeeData[3];
+    employee.jam = employeeData[7]; // number
+    return employee;
+  }
+
+  if (employeeLength == 11) {
+    employee.name = employeeData[4];
+    employee.remSem = employeeData[5]; // number
+    employee.rem = employeeData[6]; // number
+    employee.pis = employeeData[1];
+    employee.baseSalPrev = employeeData[9]; // number
+    employee.admissao = employeeData[2];
+    employee.conSeg = employeeData[8]; // number
+    employee.cat = employeeData[3];
+    employee.ocor = employeeData[10]; // number
+    employee.dataCodMov = employeeData[0];
+    employee.cbo = '0';
+    employee.jam = employeeData[7]; // number
+    return employee;
+  }
+  return employee;
+};
+
+module.exports = {
+  analyzer,
+};
