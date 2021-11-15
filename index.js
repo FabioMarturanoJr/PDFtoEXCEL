9eaacfda43f088c93188928cfca79a590d0d97e5
Fabio Marturano <fabiocmjor@gmail.com>
2021-11-15 04:04:22 -0300

    Mensagem mais amigavel para arquivo nao encontrado

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
-const pdfPath = path.resolve(__dirname, 'PDF', 'Relatório RE.pdf');
+const nameFile = 'Relatório RE.pdf'
+
+const pdfPath = path.resolve(__dirname, 'PDF', nameFile);
 
 const ReadPDF = require('./models/pdfReader');
+
+const errorFile = 'no such file or directory'
 
 try {
   ReadPDF(pdfPath);  
 } catch (e) {
-  console.log(e.message);
+  if (e.message.includes(errorFile)) console.log(`Arquivo não encontrado, verifique na pasta "PDF", Arquivo esperado: "${nameFile}"`);
+  else console.log(e.message);
 }
